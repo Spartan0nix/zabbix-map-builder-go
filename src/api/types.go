d94af3fbce83dcd2c5b5734721b2033272f38dc8
Spartan0nix <l.humblot74@gmail.com>
2022-02-18 23:06:19 +0100

    Update map types & add Host type

--- src/api/types.go
+++ src/api/types.go
@@ -20,9 +20,41 @@ type Response struct {
 	Id       int         `json:"id"`
 }
 
+type Map_selement struct {
+	Selementid      string        `json:"selementid"`
+	Elementtype     string        `json:"elementtype"`
+	Elements        []interface{} `json:"elements"`
+	Iconid_off      string        `json:"iconid_off"`
+	Label           string        `json:"label"`
+	Label_location  string        `json:"label_location"`
+	Inherited_label string        `json:"inherited_label"`
+	Label_type      string        `json:"label_type"`
+	Color           string        `json:"color"`
+	Linktriggers    []string      `json:"linktriggers"`
+}
+
+type Map_link struct {
+	Link_id      string   `json:"linkid"`
+	Sysmapid     string   `json:"sysmapid"`
+	Selementid1  string   `json:"selementid1"`
+	Selementid2  string   `json:"selementid2"`
+	Color        string   `json:"color"`
+	Label        string   `json:"label"`
+	Linktriggers []string `json:"linktriggers"`
+}
+
 type Map struct {
-	Sysmapid string `json:"sysmapid"`
-	Name     string `json:"name"`
-	Height   string `json:"height"`
-	Width    string `json:"width"`
+	Sysmapid  string         `json:"sysmapid"`
+	Name      string         `json:"name"`
+	Height    string         `json:"height"`
+	Width     string         `json:"width"`
+	Selements []Map_selement `json:"selements"`
+	Links     []Map_link     `json:"links"`
+}
+
+type Host struct {
+	Hostid      string `json:"hostid"`
+	Host        string `json:"host"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }

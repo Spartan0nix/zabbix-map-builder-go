df4aaa939be9addd108f4747c184d2d755ea6a54
Sparta0nix <dev.humblole@gmail.com>
2023-07-01 15:45:19 +0200

    fix(app): add function to generate root commande && update checkRequiredFlag return data

--- cmd/root.go
+++ cmd/root.go
@@ -21,60 +21,69 @@ var Debug bool
 var DryRun bool
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&Name, "name", "", "name of map")
-	rootCmd.PersistentFlags().StringVarP(&File, "file", "f", "", "input file")
-	rootCmd.PersistentFlags().StringVarP(&OutFile, "output", "o", "", "output the parameters used to create the map")
-	rootCmd.PersistentFlags().StringVarP(&Color, "color", "c", "000000", "color in hexadecimal used for the links between each hosts")
-	rootCmd.PersistentFlags().StringVar(&TriggerColor, "trigger-color", "DD0000", "color in hexadecimal used for the links between each hosts when a trigger is in problem state")
-	rootCmd.PersistentFlags().BoolSliceVar(&StackHosts, "stack-hosts", []bool{true}, "connect multiple links to a single host. If set to false, if mapping will have is own hosts. This can be useful for infrastructure with redundant connexion")
-	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "v", false, "enable debug logging verbosity")
-	rootCmd.PersistentFlags().BoolVar(&DryRun, "dry-run", false, "output to the shell the map definition without created it on the server")
-
-	rootCmd.MarkPersistentFlagRequired("name")
-	rootCmd.MarkPersistentFlagRequired("file")
-
 	// Init a new global logger
 	GlobalLogger = logging.NewLogger(logging.Warning)
 }
 
-var rootCmd = &cobra.Command{
-	Use:   "",
-	Short: "Build a zabbix map using the given host mapping.",
-	Long:  "This CLI tool is used to help administrator build a zabbix map using the given host mappings (network devices, etc.).",
-	PreRun: func(cmd *cobra.Command, args []string) {
-		// Check if the file flag was set correctly.
-		checkRequiredFlag(Name, File)
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		// Enable debug logger level.
-		if Debug {
-			GlobalLogger.Level = logging.Debug
-		}
+// newRootCmd is used to generate the root command for the CLI
+func newRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "",
+		Short: "Build a zabbix map using the given host mapping.",
+		Long:  "This CLI tool is used to help administrator build a zabbix map using the given host mappings (network devices, etc.).",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			// Check if the file flag was set correctly.
+			if err := checkRequiredFlag(Name, File); err != "" {
+				GlobalLogger.Error(err)
+				os.Exit(1)
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			// Enable debug logger level.
+			if Debug {
+				GlobalLogger.Level = logging.Debug
+			}
 
-		// Retrieve the required environment variables.
-		options, err := app.GetEnvironmentVariables()
-		if err != nil {
-			GlobalLogger.Error("error when reading the required environment variables", fmt.Sprintf("reason : %s", err))
-			os.Exit(1)
-		}
+			// Retrieve the required environment variables.
+			options, err := app.GetEnvironmentVariables()
+			if err != nil {
+				GlobalLogger.Error("error when reading the required environment variables", fmt.Sprintf("reason : %s", err))
+				os.Exit(1)
+			}
 
-		options.Name = Name
-		options.OutFile = OutFile
-		options.Color = Color
-		options.StackHosts = StackHosts[0]
-		options.TriggerColor = TriggerColor
-		options.DryRun = DryRun
+			options.Name = Name
+			options.OutFile = OutFile
+			options.Color = Color
+			options.StackHosts = StackHosts[0]
+			options.TriggerColor = TriggerColor
+			options.DryRun = DryRun
 
-		// Run the application.
-		err = app.RunApp(File, options)
-		if err != nil {
-			GlobalLogger.Error("error when executing command", err)
-			os.Exit(1)
-		}
-	},
+			// Run the application.
+			err = app.RunApp(File, options)
+			if err != nil {
+				GlobalLogger.Error("error when executing command", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	// Set all the persistent flag
+	cmd.PersistentFlags().StringVar(&Name, "name", "", "name of map")
+	cmd.PersistentFlags().StringVarP(&File, "file", "f", "", "input file")
+	cmd.PersistentFlags().StringVarP(&OutFile, "output", "o", "", "output the parameters used to create the map")
+	cmd.PersistentFlags().StringVarP(&Color, "color", "c", "000000", "color in hexadecimal used for the links between each hosts")
+	cmd.PersistentFlags().StringVar(&TriggerColor, "trigger-color", "DD0000", "color in hexadecimal used for the links between each hosts when a trigger is in problem state")
+	cmd.PersistentFlags().BoolSliceVar(&StackHosts, "stack-hosts", []bool{true}, "connect multiple links to a single host. If set to false, if mapping will have is own hosts. This can be useful for infrastructure with redundant connexion")
+	cmd.PersistentFlags().BoolVarP(&Debug, "debug", "v", false, "enable debug logging verbosity")
+	cmd.PersistentFlags().BoolVar(&DryRun, "dry-run", false, "output to the shell the map definition without created it on the server")
+	cmd.MarkPersistentFlagRequired("name")
+	cmd.MarkPersistentFlagRequired("file")
+
+	return cmd
 }
 
 func Execute() {
+	rootCmd := newRootCmd()
 	err := rootCmd.Execute()
 	if err != nil {
 		log.Fatalf("Error when during command initialization.\nReason : %v", err)
@@ -82,19 +91,18 @@ func Execute() {
 }
 
 // checkRequiredFlag is used to validate the required flags.
-func checkRequiredFlag(name string, file string) {
+func checkRequiredFlag(name string, file string) string {
 	if name == "" {
-		GlobalLogger.Error("'name' flag is required and cannot be empty")
-		os.Exit(1)
+		return "'name' flag is required and cannot be empty"
 	}
 
 	if file == "" {
-		GlobalLogger.Error("'file' flag is required and cannot be empty")
-		os.Exit(1)
+		return "'file' flag is required and cannot be empty"
 	}
 
 	if _, err := os.Stat(file); err != nil {
-		GlobalLogger.Error(fmt.Sprintf("error while reading file '%s'.", file), err)
-		os.Exit(1)
+		return fmt.Sprintf("error while reading file '%s'.", file)
 	}
+
+	return ""
 }
